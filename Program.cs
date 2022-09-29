eca89bf1363e8e072ec81bb4a5f4062bb82e431d
Gvido <gaca6817@gmail.com>
2022-09-29 14:42:30 +0300

    Added tests for Employee class

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee
+namespace IfEmployee
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var employee = new Employee();
         }
     }
 }
