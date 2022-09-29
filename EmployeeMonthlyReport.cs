5e402cb449e3932b86b03c27322300e6851b9741
Gvido <gaca6817@gmail.com>
2022-09-29 14:52:08 +0300

    Added tests for EmployeeMonthlyReport class

--- EmployeeMonthlyReport.cs
+++ EmployeeMonthlyReport.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IfEmployee
+﻿namespace IfEmployee
 {
     public class EmployeeMonthlyReport
     {
